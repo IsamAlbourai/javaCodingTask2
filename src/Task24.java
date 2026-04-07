567e79fd84b6df7b11516a84c1aae7878275c84a
Isam <123lightningstrike@gmail.com>
2026-04-07 15:08:23 +0400

    Task 24 completed

--- src/Task24.java
+++ src/Task24.java
@@ -1,2 +1,16 @@
 public class Task24 {
+    public static void main(String[] args) {
+
+        Integer n = 19;
+
+        Boolean result;
+
+        if (n % 20 == 18 || n % 20 == 19) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        System.out.println(result);
+    }
 }
