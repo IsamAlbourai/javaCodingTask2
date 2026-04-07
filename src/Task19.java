62aa63b592795b4fe0541f56319700f129ce966e
Isam <123lightningstrike@gmail.com>
2026-04-07 14:59:13 +0400

    Task 19 completed

--- src/Task19.java
+++ src/Task19.java
@@ -1,2 +1,22 @@
 public class Task19 {
+    public static void main(String[] args) {
+
+        Integer a = 0;
+        Integer b = 0;
+        Integer c = 0;
+
+        Integer result;
+
+        if (a == 2 && b == 2 && c == 2) {
+            result = 10;
+        } else if (a == b && b == c) {
+            result = 5;
+        } else if (b != a && c != a) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+
+        System.out.println(result);
+    }
 }
