d6e8becb3b40b3ff3dcae2f0e6ebd693c996116e
Isam <123lightningstrike@gmail.com>
2026-04-07 15:14:29 +0400

    Task 28 completed + all tasks are finalized

--- src/Task28.java
+++ src/Task28.java
@@ -1,2 +1,27 @@
 public class Task28 {
+    public static void main(String[] args) {
+
+        Integer die1 = 6;
+        Integer die2 = 6;
+        boolean noDoubles = true;
+
+        int sum;
+
+        if (!noDoubles) {
+            sum = die1 + die2;
+        } else {
+            if (die1 != die2) {
+                sum = die1 + die2;
+            } else {
+                if (die1 == 6) {
+                    die1 = 1;
+                } else {
+                    die1 = die1 + 1;
+                }
+                sum = die1 + die2;
+            }
+        }
+
+        System.out.println(sum);
+    }
 }
