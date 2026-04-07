5e20bb033d9f77fa257a2c3cacc2869952a80d28
Isam <123lightningstrike@gmail.com>
2026-04-07 15:05:08 +0400

    Task 22 completed

--- src/Task22.java
+++ src/Task22.java
@@ -1,2 +1,25 @@
 public class Task22 {
+    public static void main(String[] args) {
+
+        Integer day = 0;
+        Boolean vacation = false;
+
+        String result;
+
+        if (day >= 1 && day <= 5) {
+            if (vacation) {
+                result = "10:00";
+            } else {
+                result = "7:00";
+            }
+        } else {
+            if (vacation) {
+                result = "off";
+            } else {
+                result = "10:00";
+            }
+        }
+
+        System.out.println(result);
+    }
 }
