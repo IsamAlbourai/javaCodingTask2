e6394789ddb747270777f42f1523857fe8d45de2
Isam <123lightningstrike@gmail.com>
2026-04-07 15:09:59 +0400

    Task 25 completed

--- src/Task25.java
+++ src/Task25.java
@@ -1,2 +1,24 @@
 public class Task25 {
+    public static void main(String[] args) {
+
+        Boolean isMorning = false;
+        Boolean isMom = false;
+        Boolean isAsleep = false;
+
+        Boolean result;
+
+        if (isAsleep) {
+            result = false;
+        } else if (isMorning) {
+            if (isMom) {
+                result = true;
+            } else {
+                result = false;
+            }
+        } else {
+            result = true;
+        }
+
+        System.out.println(result);
+    }
 }
