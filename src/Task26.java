5bbb13affa88ab5e7f9b27ca903351116bbdc920
Isam <123lightningstrike@gmail.com>
2026-04-07 15:11:24 +0400

    Task 26 completed

--- src/Task26.java
+++ src/Task26.java
@@ -1,2 +1,20 @@
 public class Task26 {
+    public static void main(String[] args) {
+
+        Integer n = 15;
+
+        String result;
+
+        if (n % 3 == 0 && n % 5 == 0) {
+            result = "FizzBuzz!";
+        } else if (n % 3 == 0) {
+            result = "Fizz!";
+        } else if (n % 5 == 0) {
+            result = "Buzz!";
+        } else {
+            result = n + "!";
+        }
+
+        System.out.println(result);
+    }
 }
