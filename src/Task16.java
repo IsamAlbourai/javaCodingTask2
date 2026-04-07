4f27fca34c856b286ce0507413e8c9b34e10ff6b
Isam <123lightningstrike@gmail.com>
2026-04-07 14:45:18 +0400

    Task 16 completed

--- src/Task16.java
+++ src/Task16.java
@@ -1,2 +1,20 @@
 public class Task16 {
+    public static void main(String[] args) {
+
+        String str = "fib";
+
+        String result;
+
+        if (str.startsWith("f") && str.endsWith("b")) {
+            result = "FizzBuzz";
+        } else if (str.startsWith("f")) {
+            result = "Fizz";
+        } else if (str.endsWith("b")) {
+            result = "Buzz";
+        } else {
+            result = str;
+        }
+
+        System.out.println(result);
+    }
 }
