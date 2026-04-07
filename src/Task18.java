9add0c5de010837feb1fc8854298d9cacb0384ec
Isam <123lightningstrike@gmail.com>
2026-04-07 14:53:01 +0400

    Task 18 completed

--- src/Task18.java
+++ src/Task18.java
@@ -1,2 +1,22 @@
 public class Task18 {
+    public static void main(String[] args) {
+
+        Integer a = 1;
+        Integer b = 7;
+        Integer c = 11;
+
+        Boolean result;
+
+        if (a - b >= 10 || b - a >= 10) {
+            result = true;
+        } else if (a - c >= 10 || c - a >= 10) {
+            result = true;
+        } else if (b - c >= 10 || c - b >= 10) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        System.out.println(result);
+    }
 }
