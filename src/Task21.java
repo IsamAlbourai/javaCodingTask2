e1eca8aadcc4e34c733c7bf4d071f76a7d50c8b2
Isam <123lightningstrike@gmail.com>
2026-04-07 15:03:05 +0400

    Task 21 completed

--- src/Task21.java
+++ src/Task21.java
@@ -1,2 +1,25 @@
 public class Task21 {
+    public static void main(String[] args) {
+
+        Integer temp = 95;
+        Boolean isSummer = true;
+
+        Boolean result;
+
+        if (isSummer) {
+            if (temp >= 60 && temp <= 100) {
+                result = true;
+            } else {
+                result = false;
+            }
+        } else {
+            if (temp >= 60 && temp <= 90) {
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+
+        System.out.println(result);
+    }
 }
