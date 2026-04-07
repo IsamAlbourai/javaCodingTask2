6a933f0dc832ce709b49c505c747bf4c0498d923
Isam <123lightningstrike@gmail.com>
2026-04-07 15:01:26 +0400

    Task 20 completed

--- src/Task20.java
+++ src/Task20.java
@@ -1,2 +1,23 @@
 public class Task20 {
+    public static void main(String[] args) {
+
+        Integer a = 12;
+        Integer b = 23;
+
+        Integer aLeft = a / 10;
+        Integer aRight = a % 10;
+
+        Integer bLeft = b / 10;
+        Integer bRight = b % 10;
+
+        Boolean result;
+
+        if (aLeft == bLeft || aLeft == bRight || aRight == bLeft || aRight == bRight) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        System.out.println(result);
+    }
 }
