f5ad46827a296b02e3429c4869bf87488926bdd4
Isam <123lightningstrike@gmail.com>
2026-04-07 15:07:09 +0400

    Task 23 completed

--- src/Task23.java
+++ src/Task23.java
@@ -1,2 +1,16 @@
 public class Task23 {
+    public static void main(String[] args) {
+
+        Integer n = 23;
+
+        Boolean result;
+
+        if (n % 11 == 0 || n % 11 == 1) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        System.out.println(result);
+    }
 }
