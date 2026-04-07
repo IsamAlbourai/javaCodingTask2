9ad389c3ab6b23a5296c9e3a75014d0dad69c9da
Isam <123lightningstrike@gmail.com>
2026-04-07 15:12:41 +0400

    Task 27 completed

--- src/Task27.java
+++ src/Task27.java
@@ -1,2 +1,27 @@
 public class Task27 {
+    public static void main(String[] args) {
+
+        Integer a = 5;
+        Integer b = 5;
+        Integer c = 7;
+        boolean equalOk = true;
+
+        boolean result;
+
+        if (equalOk) {
+            if (a <= b && b <= c) {
+                result = true;
+            } else {
+                result = false;
+            }
+        } else {
+            if (a < b && b < c) {
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+
+        System.out.println(result);
+    }
 }
